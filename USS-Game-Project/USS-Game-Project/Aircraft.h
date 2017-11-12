467bdd195984c2afa15f06fbb521696a822b0153
Sachin Jangra <sjangra2305@gmail.com>
2017-11-12 21:32:52 +0530

    World Setup

---
+++ USS-Game-Project/USS-Game-Project/Aircraft.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ResourceIdentifiers.h"
+class Aircraft : public Entity
+{
+public:
+	enum Type
+	{
+		Eagle,
+		Raptor,
+	};
+
+public:
+	Aircraft(Type type, const TextureHolder& textures);
+
+private:
+	virtual void		drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+
+private:
+	Type				mType;
+	sf::Sprite			mSprite;
+};
---
+++ USS-Game-Project/USS-Game-Project/World.h
@@ -0,0 +1,42 @@
+#pragma once
+// Forward declaration
+namespace sf
+{
+	class RenderWindow;
+}
+
+class World : private sf::NonCopyable
+{
+public:
+	explicit							World(sf::RenderWindow& window);
+	void								update(sf::Time dt);
+	void								draw();
+
+
+private:
+	void								loadTextures();
+	void								buildScene();
+
+
+private:
+	enum Layer
+	{
+		Background,
+		Air,
+		LayerCount
+	};
+
+
+private:
+	sf::RenderWindow&					mWindow;
+	sf::View							mWorldView;
+	TextureHolder						mTextures;
+
+	SceneNode							mSceneGraph;
+	std::array<SceneNode*, LayerCount>	mSceneLayers;
+
+	sf::FloatRect						mWorldBounds;
+	sf::Vector2f						mSpawnPosition;
+	float								mScrollSpeed;
+	Aircraft*							mPlayerAircraft;
+};
